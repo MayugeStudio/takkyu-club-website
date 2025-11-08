1a3c422920e721ff747397170e0552cb955570e1
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-11-08 15:07:13 +0900

    スライドショー

--- main.js
+++ main.js
@@ -33,3 +33,14 @@ window.addEventListener('scroll', function() {
         }
     });
 });
+
+const images = document.querySelectorAll('.slideshow img');
+let index = 0;
+
+setInterval(() => {
+  images[index].classList.remove('active');
+  index = (index + 1) % images.length;
+  images[index].classList.add('active');
+}, 3000);
+
+
